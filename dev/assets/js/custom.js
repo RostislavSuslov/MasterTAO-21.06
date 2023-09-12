84918a5c22c11c5c739dcbc51f6618a08e7fc806
RostSuslov <suslovros@gmail.com>
2023-09-12 19:58:34 +0300

    add other slider

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,39 +1,38 @@
 const burger = document.querySelector('#hamburger');
 
-burger.addEventListener('click', function(){
+burger.addEventListener('click', function() {
     burger.classList.toggle('is-active')
 })
 
 
 
 $(document).ready(function() {
-  
-      mobileOnlySlider("#advanages-slider", true, false, 1024);
+    $('.first-screen').slick();
 
-function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
-  var slider = $($slidername);
-  var settings = {
-    mobileFirst: true,
-    dots: $dots,
-    arrows: $arrows,
-    responsive: [
-      {
-        breakpoint: $breakpoint,
-        settings: "unslick"
-      }
-    ]
-  };
 
-  slider.slick(settings);
+    mobileOnlySlider("#advanages-slider", true, false, 1024);
 
-  $(window).on("resize", function () {
-    if ($(window).width() > $breakpoint) {
-      return;
-    }
-    if (!slider.hasClass("slick-initialized")) {
-      return slider.slick(settings);
-    }
-  });
-} // Mobile Only Slider
+    function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+        var slider = $($slidername);
+        var settings = {
+            mobileFirst: true,
+            dots: $dots,
+            arrows: $arrows,
+            responsive: [{
+                breakpoint: $breakpoint,
+                settings: "unslick"
+            }]
+        };
+
+        slider.slick(settings);
+
+        $(window).on("resize", function() {
+            if ($(window).width() > $breakpoint) {
+                return;
+            }
+            if (!slider.hasClass("slick-initialized")) {
+                return slider.slick(settings);
+            }
+        });
+    } // Mobile Only Slider
 });
-
