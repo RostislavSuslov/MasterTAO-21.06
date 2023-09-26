0738e8a6ee9ee6599fa6c154dc8d10b4df2e4852
RostSuslov <suslovros@gmail.com>
2023-09-26 20:10:37 +0300

    install arrows for carousel-template

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,67 +1,3 @@
-const burger = document.querySelector('#hamburger');
-
-burger.addEventListener('click', function() {
-    burger.classList.toggle('is-active')
-})
-
-document.getElementById('fileInput').onchange = function () {
-    //short name
-    // document.getElementById('file-name').innerHTML = this.files[0].name;
-    document.getElementById('file-name').innerHTML = this.value;
-};
-
-
-
- 
-
- 
-const checkButtons = document.querySelectorAll('.check-button');
-console.log(checkButtons);
-
-checkButtons.forEach((item, index) => {
-  const colParent = item.closest('.col-lg-6');
-   colParent ? colParent.classList.add('padding-bottom') : null;
-
-   index === 5 ? item.classList.add("bg-warning") : null;
-});
-
- 
-// checkButtons.forEach(item => {
-//     item.closest('.col-lg-6').classList.add('padding-bottom')
-// })
-
-document.querySelectorAll('.check-button')[1].classList.add("this-is-legasy-code")
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function() {
     $('.first-screen').slick({
         infinite: true,
@@ -104,4 +40,48 @@ $(document).ready(function() {
             }
         });
     } // Mobile Only Slider
+
+
+ 
+    // Выбираем все элементы с классом 'carousel-template' и для каждого из них выполняем следующую функцию
+$('.carousel-template').each(function () {
+    var slider = $(this); // Получаем текущий элемент с классом 'carousel-template' и сохраняем его в переменной slider
+    
+    // Инициализируем плагин Slick Carousel для текущего элемента
+    slider.slick({
+        infinite: true, // Бесконечная прокрутка карусели
+        slidesToShow: 3, // Отображать 3 слайда одновременно
+        slidesToScroll: 1, // Прокручивать по 1 слайду за раз
+
+        // Настройка стрелок для навигации по карусели:
+        prevArrow: slider.closest('.container').find('.slick-prev'), // Ищем элемент с классом 'slick-prev' в родительском контейнере c классом .container и используем его как кнопку "назад"
+        nextArrow: slider.closest('.container').find('.slick-next'), // Ищем элемент с классом 'slick-next' в родительском контейнере c классом .container и используем его как кнопку "вперед"
+    });
 });
+});
+
+const burger = document.querySelector('#hamburger');
+burger.addEventListener('click', function() {
+    burger.classList.toggle('is-active')
+})
+
+document.getElementById('fileInput').onchange = function () {
+    //short name
+    // document.getElementById('file-name').innerHTML = this.files[0].name;
+    document.getElementById('file-name').innerHTML = this.value;
+};
+
+ 
+const checkButtons = document.querySelectorAll('.check-button');
+console.log(checkButtons);
+
+checkButtons.forEach((item, index) => {
+  const colParent = item.closest('.col-lg-6');
+   colParent ? colParent.classList.add('padding-bottom') : null;
+
+   index === 5 ? item.classList.add("bg-warning") : null;
+});
+
+document.querySelectorAll('.check-button')[1].classList.add("this-is-legasy-code")
+ 
+
